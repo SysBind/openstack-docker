3f600e62e6a7da043663a0e663e480e02df50699
Asaf Ohayon <asaf@sysbind.co.il>
2020-02-18 13:28:37 +0200

    glance entrypoint: accept registry, api or arbitary command

--- README.md
+++ README.md
@@ -8,3 +8,28 @@ No environment variables support, use volumes to configure,
 e.g: /etc/keystone/keystone.cfg
 or entire /etc/keystone
 
+## Keystone (Identity Service)
+
+_Minimum config_: 
+- /etc/keystone/keystone.cfg
+
+_Commands_:
+- Only default: runs uwsgi server with the keystone API
+
+or arbitary command
+
+## Glance (Image Service)
+_Minimum config_: 
+- /etc/glance/glance-api.conf
+
+If supplying entire /etc/glance as a volume,
+You'll also need:
+- /etc/glance/glance-api-paste.ini
+- /etc/glance/glance-registry-paste.ini
+(You can copy them from a running glance container)
+
+_Commands_:
+- api: default, run uwsgi with the glance API
+- registry
+
+or arbitary command
--- glance/entrypoint.sh
+++ glance/entrypoint.sh
@@ -4,7 +4,24 @@ if [ ! -z ${TESTING+x} ]; then
     psql --host db --tuples-only --user postgres -c "\l" | grep keystone || psql --host db --tuples-only --user postgres -c "CREATE DATABASE glance"
 fi
 
-glance-manage db_sync
+if [ ! -z ${1+x} ]; then
+    echo "got param $1"
+    case "$1" in
+	registry)
+	    glance-registry
+	    ;;
+	api)
+	    glance-manage db_sync
+	    uwsgi --http 0.0.0.0:9292 --wsgi-file /usr/bin/glance-wsgi-api
+	    ;;
+	*)
+	    echo "running arbitary command $@"
+	    $@
+	    ;;
+    esac
+else
+    glance-manage db_sync
+    uwsgi --http 0.0.0.0:9292 --wsgi-file /usr/bin/glance-wsgi-api
+fi
 
-uwsgi --http 0.0.0.0:5000 --wsgi-file /usr/bin/glance-wsgi-api
 
