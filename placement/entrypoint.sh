b4e6f8042bd279944bd684d1895c22798e814a2b
Asaf Ohayon <asaf@sysbind.co.il>
2020-02-18 14:54:49 +0200

    placement: handle command line args

--- placement/entrypoint.sh
+++ placement/entrypoint.sh
@@ -4,7 +4,21 @@ if [ ! -z ${TESTING+x} ]; then
     psql --host db --tuples-only --user postgres -c "\l" | grep placement || psql --host db --tuples-only --user postgres -c "CREATE DATABASE placement"
 fi
 
-placement-manage db sync
+echo "CMD: $@"
 
-uwsgi --http 0.0.0.0:5000 --wsgi-file /usr/bin/placement-api
+if [ ! -z ${1+x} ]; then
+    case "$1" in
+	api)
+	    placement-manage db sync
+	    uwsgi --http 0.0.0.0:5000 --wsgi-file /usr/bin/placement-api
+	    ;;
+	*)
+	    echo "running arbitary command $@"
+	    $@
+	    ;;
+	  esac
+else
+    placement-manage db sync
+    uwsgi --http 0.0.0.0:5000 --wsgi-file /usr/bin/placement-api	      
+fi	    
 
